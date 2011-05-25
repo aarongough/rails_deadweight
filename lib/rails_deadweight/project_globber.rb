c39e965f5d1be91589bd9fc964c6b3b26bf5f11d
Aaron Gough <agough@mdialog.com>
2011-05-25 18:14:52 -0400

    Added implementation of ProjectGlobber.

--- lib/rails_deadweight/project_globber.rb
+++ lib/rails_deadweight/project_globber.rb
@@ -1,9 +1,25 @@
 module RailsDeadweight
   class ProjectGlobber
     
+    RUBY_FILE_EXTENSIONS = [".rb", ".erb"]
+    
     def self.get_globbed_project(project_root)
       app_root = File.expand_path(project_root) + "/app"
-      puts app_root
+      project_files = []
+      project_glob = ""
+      
+      RUBY_FILE_EXTENSIONS.each do |extension|
+        project_files.concat Dir[app_root + "/**/*#{extension}"]
+      end
+      
+      project_files.each do |path|
+        File.open(path) do |file|
+          project_glob << file.read
+          project_glob << "\n"
+        end
+      end
+      
+      return project_glob
     end
     
   end
