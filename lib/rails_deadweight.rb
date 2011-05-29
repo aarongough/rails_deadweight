72143c5d04a6ef5af81ffe0fa53e29437150eea5
Aaron Gough <agough@mdialog.com>
2011-05-29 18:45:16 -0400

    Removed old ProjectParser class.

--- lib/rails_deadweight.rb
+++ lib/rails_deadweight.rb
@@ -1,5 +1,3 @@
-require File.dirname(__FILE__) + "/rails_deadweight/project_parser.rb"
-
 require File.dirname(__FILE__) + "/rails_deadweight/parsers/method_parser.rb"
 require File.dirname(__FILE__) + "/rails_deadweight/parsers/class_parser.rb"
 require File.dirname(__FILE__) + "/rails_deadweight/parsers/route_parser.rb"
