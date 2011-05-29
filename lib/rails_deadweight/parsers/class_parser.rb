9f8698e651f212932bdb413aa93936ef115d9d9c
Aaron Gough <agough@mdialog.com>
2011-05-29 18:37:12 -0400

    Added initial implementation of ClassParser.

--- lib/rails_deadweight/parsers/class_parser.rb
+++ lib/rails_deadweight/parsers/class_parser.rb
@@ -2,6 +2,47 @@ module RailsDeadweight
   module Parsers
     class ClassParser
       
+      CLASS_DEFINITION_PATTERN = /class\s+([A-Z]+[A-Za-z0-9]*)/
+      
+      def self.get_defined_classes(files)
+        classes = []
+        
+        files.each do |file|
+          raw_class_definitions = file[:content].scan CLASS_DEFINITION_PATTERN
+          raw_class_definitions.map! do |class_definition|
+            class_definition.to_s
+          end
+          
+          raw_class_definitions.each do |definition|
+            classes << {
+              :file_path => file[:path],
+              :name => definition
+            }
+          end
+        end
+        
+        return classes
+      end
+      
+      def self.count_class_references(files, method_name)
+        class_reference_pattern = Regexp.new("(class)?\\s*(#{method_name})")
+        class_references = []
+        
+        files.each do |file|
+          class_references.concat file[:content].scan class_reference_pattern
+        end
+
+        class_references = class_references.map do |class_reference|
+          class_reference.to_s
+        end
+
+        class_references = class_references.reject do |class_reference|
+          class_reference.match /^class/
+        end
+
+        return class_references.count
+      end
+      
     end
   end
 end
--- spec/parsers/class_parser_spec.rb
+++ spec/parsers/class_parser_spec.rb
@@ -3,28 +3,34 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe RailsDeadweight::Parsers::ClassParser do
   describe ".get_defined_classes" do
     it "should detect a simple class definition" do
-      example_code = <<-EOD
-        class Foo
-          # no methods
-        end
-      EOD
+      example_files = [{
+        :file_path => "/foo/test",
+        :content => <<-EOD
+          class Foo
+            # no methods
+          end
+        EOD
+      }]
       
-      classes = RailsDeadweight::Parsers::ClassParser.get_defined_classes(example_code)
+      classes = RailsDeadweight::Parsers::ClassParser.get_defined_classes(example_files).map{|x| x[:name]}
       classes.should include "Foo"
     end
     
     it "should detect multiple class definitions" do
-      example_code = <<-EOD
-        class Foo
-          # no methods
-        end
-        
-        class Bar
-          # no methods
-        end
-      EOD
+      example_files = [{
+        :file_path => "/foo/test",
+        :content => <<-EOD
+          class Foo
+            # no methods
+          end
+
+          class Bar
+            # no methods
+          end
+        EOD
+      }]
       
-      classes = RailsDeadweight::Parsers::ClassParser.get_defined_classes(example_code)
+      classes = RailsDeadweight::Parsers::ClassParser.get_defined_classes(example_files).map{|x| x[:name]}
       classes.should include "Bar"
       classes.should include "Foo"
     end
@@ -32,38 +38,50 @@ describe RailsDeadweight::Parsers::ClassParser do
   
   describe ".count_class_references" do
     it "should detect a reference to the class" do
-      example_code = <<-EOD
-        FooClass
-      EOD
+      example_files = [{
+        :file_path => "/foo/test",
+        :content => <<-EOD
+          FooClass
+        EOD
+      }]
       
-      count = RailsDeadweight::Parsers::ClassParser.count_class_references(example_code, "FooClass")
+      count = RailsDeadweight::Parsers::ClassParser.count_class_references(example_files, "FooClass")
       count.should == 1
     end
     
     it "should detect the usage of a class method without params" do
-      example_code = <<-EOD
-        bar = FooClass.new
-      EOD
+      example_files = [{
+        :file_path => "/foo/test",
+        :content => <<-EOD
+          bar = FooClass.new
+        EOD
+      }]
       
-      count = RailsDeadweight::Parsers::ClassParser.count_class_references(example_code, "FooClass")
+      count = RailsDeadweight::Parsers::ClassParser.count_class_references(example_files, "FooClass")
       count.should == 1
     end
     
     it "should detect the usage of a class method with params" do
-      example_code = <<-EOD
-        bar = FooClass.foo_bar(1, 2)
-      EOD
+      example_files = [{
+        :file_path => "/foo/test",
+        :content => <<-EOD
+          bar = FooClass.foo_bar(1, 2)
+        EOD
+      }]
       
-      count = RailsDeadweight::Parsers::ClassParser.count_class_references(example_code, "FooClass")
+      count = RailsDeadweight::Parsers::ClassParser.count_class_references(example_files, "FooClass")
       count.should == 1
     end
     
     it "should not detect misspelled class reference" do
-      example_code = <<-EOD
-        FoodClass
-      EOD
+      example_files = [{
+        :file_path => "/foo/test",
+        :content => <<-EOD
+          FoodClass
+        EOD
+      }]
       
-      count = RailsDeadweight::Parsers::ClassParser.count_class_references(example_code, "FooClass")
+      count = RailsDeadweight::Parsers::ClassParser.count_class_references(example_files, "FooClass")
       count.should == 0
     end
   end
