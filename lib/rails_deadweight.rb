dc67feee97ec46a8f1e980a25965e2e04f9492ea
Aaron Gough <agough@mdialog.com>
2011-05-29 18:44:33 -0400

    Added initial implementation of route parser.

--- lib/rails_deadweight.rb
+++ lib/rails_deadweight.rb
@@ -1,5 +1,4 @@
 require File.dirname(__FILE__) + "/rails_deadweight/project_parser.rb"
-require File.dirname(__FILE__) + "/rails_deadweight/project_routes.rb"
 
 require File.dirname(__FILE__) + "/rails_deadweight/parsers/method_parser.rb"
 require File.dirname(__FILE__) + "/rails_deadweight/parsers/class_parser.rb"
--- lib/rails_deadweight/parsers/route_parser.rb
+++ lib/rails_deadweight/parsers/route_parser.rb
@@ -2,6 +2,26 @@ module RailsDeadweight
   module Parsers
     class RouteParser
       
+      def self.count_routes_for_action(routes_string, method_name)
+        unless routes_string.include? ":action"
+          raise "\n\n** Could not get project routes. Please make sure you're running rails_deadweight from within your application's root directory **\n\n"
+        end
+
+        route_pattern = Regexp.new(":action=>\"#{method_name}\"")
+        return routes_string.scan(route_pattern).count
+      end
+      
+      def self.count_routes_for_controller(routes_string, controller_name)
+        controller_name = controller_name.gsub("Controller", "").downcase
+        
+        unless routes_string.include? ":controller"
+          raise "\n\n** Could not get project routes. Please make sure you're running rails_deadweight from within your application's root directory **\n\n"
+        end
+
+        route_pattern = Regexp.new(":controller=>\"#{controller_name}\"")
+        return routes_string.scan(route_pattern).count
+      end
+      
     end
   end
 end
