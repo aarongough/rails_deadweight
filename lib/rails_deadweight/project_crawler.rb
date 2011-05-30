b396e88cd78cabe8f2261dd9b8bd547f86fb07cd
Aaron Gough <agough@mdialog.com>
2011-05-30 11:01:37 -0400

    Added .builder files to the list of files to be treated as Ruby files.

--- lib/rails_deadweight/project_crawler.rb
+++ lib/rails_deadweight/project_crawler.rb
@@ -1,7 +1,7 @@
 module RailsDeadweight
   class ProjectCrawler
     
-    RUBY_FILE_EXTENSIONS = [".rb", ".erb", ".rjs", ".rhtml"]
+    RUBY_FILE_EXTENSIONS = [".rb", ".erb", ".rjs", ".rhtml", ".builder"]
     
     def self.get_project_files(root_path)
       files = self.get_ruby_files(root_path + "/app")
