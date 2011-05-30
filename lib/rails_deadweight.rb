d8577207f743aaaf4d73a1738689584abec4530e
Aaron Gough <agough@mdialog.com>
2011-05-29 20:27:37 -0400

    First working version of beautiful CLI.

--- lib/rails_deadweight.rb
+++ lib/rails_deadweight.rb
@@ -3,3 +3,4 @@ require File.dirname(__FILE__) + "/rails_deadweight/parsers/class_parser.rb"
 require File.dirname(__FILE__) + "/rails_deadweight/parsers/route_parser.rb"
 
 require File.dirname(__FILE__) + "/rails_deadweight/project_crawler.rb"
+require File.dirname(__FILE__) + "/rails_deadweight/utilities.rb"
---
+++ lib/rails_deadweight/utilities.rb
@@ -0,0 +1,20 @@
+module RailsDeadweight
+  module Utilities
+    
+    def color_puts(string, color = :white)
+      case color
+        when :white then print( "\e[37m" )
+        when :red then print( "\e[31m" )
+        when :green then print( "\e[32m" )
+        when :grey then print( "\e[90m")
+      end
+      puts string + "\e[0m"
+    end
+    
+    def progress_report(items_to_process, processed_items)
+      print "\e[1A\e[K"
+      puts "Progress: #{(processed_items.to_f / items_to_process.to_f * 100).to_i}%"
+    end
+    
+  end
+end
